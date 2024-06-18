d0b62d78061b6d8a5cef0ed659f29ec98fb2e17e
Bibekbista12 <Bistabibek80@gmail.com>
2024-06-18 16:31:18 +0545

    django project setup

---
+++ config/settings/env.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y#t(e5a6#x-qvc4%byte@&v&z5jp0ppq+q%@!@a89fckk4#a5g'
+
+
+ALLOWED_HOSTS = ["*"]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
